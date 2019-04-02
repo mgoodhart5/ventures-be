937d26b66a2e055214d095b1725139f0988e17ab
Mary Goodhart <mary.loves.pry@gmail.com>
2019-04-02 16:40:20 -0600

    Adds should matcher config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,6 +30,18 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+
+    # Choose one or more libraries:
+    with.library :active_record
+    with.library :active_model
+    with.library :action_controller
+    # Or, choose the following (which implies all of the above):
+    with.library :rails
+  end
+end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
