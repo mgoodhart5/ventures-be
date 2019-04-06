08469d3405593925d49498d42b88454c7b087a3c
Mary Goodhart <mary.loves.pry@gmail.com>
2019-04-06 16:39:08 -0600

    Adds route and method for updating user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,18 @@
 class Api::V1::UsersController < ApplicationController
+  
   def show
     render json: UserSerializer.new(User.find(params[:id]))
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    render json: UserSerializer.new(user), status: :ok
+  end
+
+  private
+
+  def user_params
+    params.permit(:name)
   end
 end
