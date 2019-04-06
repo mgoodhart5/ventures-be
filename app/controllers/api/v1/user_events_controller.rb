0241da0e3f16b3db4583b5984be781bf6d3e19c9
Anna Smolentzov <annasmolentzov@gmail.com>
2019-04-06 16:11:10 -0600

    Updates UserEvents controller to account for optional status parameter

--- app/controllers/api/v1/user_events_controller.rb
+++ app/controllers/api/v1/user_events_controller.rb
@@ -3,7 +3,11 @@ class Api::V1::UserEventsController < ApplicationController
   
   def create
     user = User.find(params[:id])
-    user.user_events.create(event_id: params[:event_id].to_i)
+    if params[:status] && params[:status] == 'attending'
+      user.user_events.create(event_id: params[:event_id].to_i, status: 'attending')
+    else
+      user.user_events.create(event_id: params[:event_id].to_i)
+    end
     render json: MyEventSerializer.new(Event.find(params[:event_id]), { params: { user_id: user.id } }), status: :created
   end
   
