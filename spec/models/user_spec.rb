719f968a33f2bded27725da8854ac0af3cfec614
Mary Goodhart <mary.loves.pry@gmail.com>
2019-04-06 15:15:28 -0600

    Adds bio and validation to user, fixes corresponding errors

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,8 +5,9 @@ describe User do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:email) }
     it { should validate_presence_of(:image_url) }
+    it { should validate_presence_of(:bio) }
   end
-  
+
   describe 'Relationships' do
     it { should have_many(:events).through(:user_events)}
   end
--- spec/requests/api/v1/users_request_spec.rb
+++ spec/requests/api/v1/users_request_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 describe 'Users API' do
   before(:each) do
-    @user = User.create(name: "User_1", email: "user_1@gmail.com", image_url: "user_1_photo.png")
+    @user = User.create(name: "User_1", email: "user_1@gmail.com", image_url: "user_1_photo.png", bio: "User_1 bio")
     @event_1 = Event.create(name: "Desert Rats Trail Running Festival", city: "Fruita", state: "CO", event_type: "running", price: 120, start_date: "04-13-2019", end_date: "04-14-2019", description: "A weekend of fun and running on the beautiful trails near Fruita, Colorado. The weekend includes a Trail Marathon & 50K races along with an Awards Party on Saturday, and 10K & Half Marathon races on Sunday.", event_url: "https://geminiadventures.com/trail-running-festival/", image_url: "https://geminiadventures.com/trail-running-festival/", video_url: "https://www.youtube.com/embed/UxVKnb8DMYQ")
     @event_2 = Event.create(name: "FIBArk", city: "Salida", state: "CO", event_type: "rafting", price: 0, start_date: "06-13-2019", end_date: "06-16-2019", description: "America's Oldest Whitewater Festival", event_url: "http://www.fibark.com/", image_url: "http://www.fibark.com/wp-content/uploads/2014/01/sup-race.jpg", video_url: "https://www.youtube.com/embed/BNrYhKYf9OE")
     @event_3 = Event.create(name: "Crested Butte Bike Week", city: "Crested Butte", state: "CO", event_type: "biking", price: 0, start_date: "06-27-2019", end_date: "06-30-2019", description: "Bike week is a multi-day celebration of all things mountain bike in the Gunnison Valley. We still have the big thigh burner of a singletrack race, the Fat Tire 40, but now CB Bike Week is more of a celebration of how mountain biking defines our community in the summer. Join us for clinics, a film festival, the Chainless Downhill World Championships, good beer, and a great party.", event_url: "http://www.cbchamber.com/chamberevents/cb-bike-week/", image_url: "https://travelcrestedbutte.com/wp-content/uploads/bike-week-feature.jpg", video_url: "https://www.youtube.com/embed/0cFSZCDMP_U")
-    
+
     @user_event_1 = UserEvent.create(user: @user, event: @event_1, status: 0)
     @user_event_2 = UserEvent.create(user: @user, event: @event_2, status: 1)
   end
@@ -21,8 +21,8 @@ describe 'Users API' do
     expect(returned_user[:attributes][:image_url]).to eq(@user.image_url)
   end
   it 'returns a single users events with attributes' do
-    get "/api/v1/users/#{@user.id}/events" 
-    
+    get "/api/v1/users/#{@user.id}/events"
+
     expect(response.status).to eq(200)
     user_events = JSON.parse(response.body, symbolize_names: true)[:data]
     expect(user_events.length).to eq(2)
