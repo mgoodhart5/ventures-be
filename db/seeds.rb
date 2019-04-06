7b17b4beaa07fc2b44144a1c261485437700e0b5
Mary Goodhart <mary.loves.pry@gmail.com>
2019-04-06 15:16:33 -0600

    Adds user bio to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,7 @@ User.destroy_all
 
 # User
 
-user = User.create(name: "Wonder Woman", email: "wonderful@gmail.com", image_url: "https://images.unsplash.com/photo-1536626420752-b144a129fde9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80")
+user = User.create(name: "Wonder Woman", email: "wonderful@gmail.com", image_url: "https://images.unsplash.com/photo-1536626420752-b144a129fde9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80", bio: "Diana is the daughter of Queen Hippolyta, the first child born on Paradise Island in the three thousand year history that the immortal Amazons lived there. The Amazons had been created around 1200 B.C. when the Greek goddesses drew forth the souls of all women who had been murdered by men and placed them on the island.")
 
 # Events
 
