5eb8e4eef0cbb4bc883a9f75eed2379091c03d6e
Mary Goodhart <mary.loves.pry@gmail.com>
2019-04-09 15:40:54 -0600

    Adds delete to cors setup

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module VenturesBe
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options, :put]
+        resource '*', headers: :any, methods: [:get, :post, :options, :put, :delete]
       end
     end
 
